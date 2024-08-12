fdd9f597461790367275c00d64f03c9fa31f3d6a
Hassan Work <hassaanmehmood68@gmail.com>
2024-08-12 21:13:39 +0500

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Sup_ChatApp.Models;
+
+namespace Sup_ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageToAll(MessagePackage msg)
+        {
+            if (msg.message != "")
+            {
+                await Clients.Others.SendAsync("ReceiveMessage", msg);
+            }
+        }
+
+        public async Task SendToSpecific(MessagePackage msg)
+        {
+            await Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", msg);
+        }
+
+    }
+}
---
+++ Models/MessagePackage.cs
@@ -0,0 +1,9 @@
+﻿namespace Sup_ChatApp.Models
+{
+    public class MessagePackage
+    {
+        public string userName { get; set; } = string.Empty;
+        public string message { get; set; } = string.Empty;
+    
+    }
+}
